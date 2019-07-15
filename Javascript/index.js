35fd8623436e0495045521ab6bc518ce101e619c
steven <stevenchiffe@gmail.com>
2019-07-15 18:17:07 +0200

    Ended flappy , have to improve some features

--- Javascript/index.js
+++ Javascript/index.js
@@ -19,23 +19,65 @@ score.src = "./Assets/Audio/score.mp3";
 
 var birdY = 150;
 var birdX = 10;
-var birdSpeed;
-var betweenTheTuyau = 85;
+var betweenThePipe = 90;
+var pipeGap = 110;
+var velocity = 1;
+var constant;
+var birdScore = 0;
+
+
+const letsFly = () => {
+  birdY -= 27;
+  fly.play();
+}
+
+var pipe = [];
+
+pipe.push({
+  x : canvas.width,
+  y : 0
+});
+
+document.addEventListener("keydown", letsFly);
 
 const loadBackground = () => {
-  background.onload = () => {
-    ctx.drawImage(background, 0, 0);
-  }
-  ground.onload = () => {
-    ctx.drawImage(ground ,0, canvas.height - ground.height);
-  }
-  bird.onload = () => {
-    ctx.drawImage(bird , birdX, birdY);
-  }
+  ctx.drawImage(background, 0, 0);
+}
+
+const displayScore = () => {
+  ctx.font = "30px Arial";
+  ctx.fillText(birdScore , 10, canvas.height - 20);
 }
 
 const Game = () => {
   loadBackground();
+  for(var x = 0; x < pipe.length; x++){
+    constant = pipeNorth.height + betweenThePipe;
+    ctx.drawImage(pipeNorth, pipe[x].x, pipe[x].y);
+    ctx.drawImage(pipeSouth, pipe[x].x, pipe[x].y + constant);
+    pipe[x].x--;
+    if(pipe[x].x == pipeGap){
+      pipe.push({
+        x : canvas.width,
+        y : Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+      });
+    }
+    if(pipe[x].x == -100){
+      pipe.splice(x, 1);
+    }
+    if(birdY >= (canvas.height - ground.height - 22) || (birdY >= pipe[x].y + constant - 20 && birdX >= pipe[x].x - 30  && birdX <= pipe[x].x + pipeNorth.width) ||  (birdY <= pipe[x].y + pipeNorth.height - 10 && birdX >= pipe[x].x - 30  && birdX <= pipe[x].x + pipeNorth.width) ){
+      location.reload();
+  }
+    if(pipe[x].x == 5){
+      birdScore++;
+      score.play();
+    }
+  }
+  ctx.drawImage(ground ,0, canvas.height - ground.height);
+  birdY += velocity;
+  ctx.drawImage(bird , birdX, birdY);
+  displayScore();
+  requestAnimationFrame(Game);
 }
 
 Game();
